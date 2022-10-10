1df7007f09ad4d891648258f155759f58b2cc8b5
Ooggle <33269056+Ooggle@users.noreply.github.com>
2022-10-10 22:43:03 +0200

    :sparkles: ggn: add error handling

--- engines/gazellegames.py
+++ engines/gazellegames.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # File name          : gazellegames.py
-# Version            : 1.2
+# Version            : 1.3
 # Author             : Ooggle (@Ooggle_)
 # Date created       : 07 Oct 2022
 
@@ -25,7 +25,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from urllib import request, parse
+from urllib import request, parse, error
 from json import loads
 
 from novaprinter import prettyPrinter
@@ -46,16 +46,43 @@ class gazellegames(object):
         "music":    "4"
     }
 
+    def error_handler(self, what, msg):
+        non_valid_api_key_msg = "API Key is not valid, Please create an API key with User and Torrents permissions from your profile."
+        no_perms_msg = "Not enough permissions from the API Key, Please create an API key with User and Torrents permissions from your profile."
+        unknown_err_msg = "Unknown error (%s), please fill an issue at https://github.com/Ooggle/qbittorrent-search-plugins/issues with your researched term" % msg
+        if "not valid" in msg:
+            final_msg = non_valid_api_key_msg
+        elif "out of scope" in msg:
+            final_msg = no_perms_msg
+        else:
+            final_msg = unknown_err_msg
+        
+        line = {"link": "-1",
+                "name": "%s: %s" % (what, final_msg),
+                "size": "-1",
+                "seeds": "0",
+                "leech": "0",
+                "engine_url": self.url,
+                "desc_link": "https://gazellegames.net/user.php?action=edit"}
+        prettyPrinter(line)
+
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
     def search(self, what, cat="all"):
         # retrieve the authkey + torrent_pass
         opener = request.build_opener()
         opener.addheaders = [("X-API-Key", self.token)]
-        with opener.open("%s/api.php?request=quick_user" % self.url) as f:
-            result_text = f.read().decode('utf-8')
+        try:
+            with opener.open("%s/api.php?request=quick_user" % self.url) as f:
+                result_text = f.read().decode('utf-8')
+        except error.URLError as e:
+            result_text = e.read().decode('utf-8')
 
-        user_result = loads(result_text)["response"]
+        user_result = loads(result_text)
+        if "error" in user_result.keys():
+            self.error_handler(parse.unquote(what), user_result["error"])
+            return
+        user_result = user_result["response"]
         authkey = user_result["authkey"]
         torrent_pass = user_result["passkey"]
 
@@ -69,10 +96,17 @@ class gazellegames(object):
 
         opener = request.build_opener()
         opener.addheaders = [("X-API-Key", self.token)]
-        with opener.open("%s/api.php?request=search" % self.url, data=data) as f:
-            result_text = f.read().decode('utf-8')
+        try:
+            with opener.open("%s/api.php?request=search" % self.url, data=data) as f:
+                result_text = f.read().decode('utf-8')
+        except error.URLError as e:
+            result_text = e.read().decode('utf-8')
 
-        search_result = loads(result_text)["response"]
+        search_result = loads(result_text)
+        if "error" in search_result.keys():
+            self.error_handler(parse.unquote(what), search_result["error"])
+            return
+        search_result = search_result["response"]
         for torrent_groups in search_result:
             for torrents in search_result[torrent_groups]["Torrents"]:
                 t = search_result[torrent_groups]["Torrents"][torrents]
