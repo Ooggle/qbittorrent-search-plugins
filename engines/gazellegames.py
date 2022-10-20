a3c94ada5822e22bb5c039f13e959543e13e16aa
Ooggle <33269056+Ooggle@users.noreply.github.com>
2022-10-20 15:13:48 +0200

    push changes

--- engines/gazellegames.py
+++ engines/gazellegames.py
@@ -6,17 +6,17 @@
 # Date created       : 07 Oct 2022
 
 # MIT License
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -56,7 +56,7 @@ class gazellegames(object):
             final_msg = no_perms_msg
         else:
             final_msg = unknown_err_msg
-        
+
         line = {"link": "-1",
                 "name": "%s: %s" % (what, final_msg),
                 "size": "-1",
